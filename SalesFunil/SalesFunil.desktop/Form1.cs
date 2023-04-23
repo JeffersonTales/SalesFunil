764869837fa1a92f1a30f7112f38bec05fff2736
Jefferson Tales Canalli <jefferson.canalli@hotmail.com>
2023-04-23 20:24:49 -0300

    secundario

--- SalesFunil/SalesFunil.desktop/Form1.cs
+++ SalesFunil/SalesFunil.desktop/Form1.cs
@@ -1,7 +1,14 @@
+using System.Security.Cryptography.X509Certificates;
+
 namespace SalesFunil.desktop {
     public partial class Form1 : Form {
         public Form1() {
+
+            #region Inicialização
+
             InitializeComponent();
+
+            #endregion
         }
     }
 }
